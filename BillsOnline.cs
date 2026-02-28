99037b526a88c0302612cd694ac627e0fe1ccba5
Tommo J. Armytage <tommojphillips@gmail.com>
2026-03-01 04:22:43 +1000

    # Fixed load order issue # Updated to v1.1.1

--- BillsOnline.cs
+++ BillsOnline.cs
@@ -6,21 +6,21 @@ public class GovOnlineFines : Mod {
     public override string ID => "BillsOnline";
     public override string Name => "Bills Online";
     public override string Author => "tommojphillips";
-    public override string Version => "1.1";
+    public override string Version => "1.1.1";
     public override string Description => "Pay bills online!";
     public override Game SupportedGames => Game.MyWinterCar;
 
     public override void ModSetup() {
 
+        SetupFunction(Setup.OnLoad, Mod_OnLoad);
+    }
+    private void Mod_OnLoad() {
         if (!ModLoader.IsModPresent("I386API")) {
             ModConsole.Error("[BillsOnline] I386 API required!");
             ModUI.ShowMessage("I386 API not installed.\nI386 API required!", Name);
             return;
         }
 
-        SetupFunction(Setup.OnLoad, Mod_OnLoad);
-    }
-    private void Mod_OnLoad() {
         Bills bills = new Bills();
         bills.load();
     }
